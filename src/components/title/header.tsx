b77ee6de901b07456ea483a30ca3da4472560942
Mike Nguyen <111297303+cooladx@users.noreply.github.com>
2025-10-15 19:43:46 -0400

    component to render h1 tag. CV-app

---
+++ src/components/title/header.tsx
@@ -0,0 +1,11 @@
+import "../../styles/header.css";
+import Separator from "../line/separator.tsx";
+
+export default function Header() {
+  return (
+    <>
+      <h1>CV-Generator</h1>
+      <Separator />
+    </>
+  );
+}
