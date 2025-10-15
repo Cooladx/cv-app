0d5f679f99c53b7f963e8c1cc9b9e920bf9052ed
Mike Nguyen <111297303+cooladx@users.noreply.github.com>
2025-10-15 19:41:04 -0400

    commponent to handle school, study, and date of user. tracks onchange and sends that change to finish component.

---
+++ src/components/educationalExperience/education.tsx
@@ -0,0 +1,148 @@
+import "../../styles/education.css";
+import Separator from "../line/separator.tsx";
+// import Button from "../button/button.tsx";
+import { useState } from "react";
+
+export type EducationData = {
+  school: string;
+  study: string;
+  from: string;
+  to: string;
+};
+
+export default function Education({
+  education,
+  setEducations,
+  index,
+}: {
+  education: EducationData;
+  setEducations: React.Dispatch<React.SetStateAction<EducationData[]>>;
+  index: number;
+}) {
+  const [school, setSchool] = useState("");
+  const [study, setStudy] = useState("");
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+
+  function addEducation() {
+    setEducations((prev) => {
+      console.log(prev);
+      return [...prev, { school: "", study: "", from: "", to: "" }];
+    });
+  }
+
+  function removeEducation() {
+    // Set education array to remove element and make it a new array.
+    setEducations((prev) => {
+      console.log(prev);
+      return prev.filter((item, i) => i !== index); // Keep everyother item except index that is deleted in array
+    });
+  }
+
+  return (
+    <>
+      <section className="education-info">
+        <label htmlFor="name">Enter your school:</label>
+        <input
+          onChange={(e) =>
+            setEducations((prev) =>
+              prev.map((item, i) => {
+                if (i === index) {
+                  // Only to CHANGE 1 property: school
+                  // If this is the index in the array of education, take changed value in the object and change.
+                  return { ...item, school: e.target.value };
+                } else {
+                  // If not returned, just leave as is for map has to return something
+                  return item;
+                }
+              }),
+            )
+          }
+          type="text"
+          className="education-input"
+          placeholder="[placeholder] College"
+        />
+        <label htmlFor="email">Enter your title of study:</label>
+        <input
+          onChange={(e) =>
+            setEducations((prev) =>
+              prev.map((item, i) => {
+                if (i === index) {
+                  // Only to CHANGE 1 property: study
+                  // If this is the index in the array of education, take changed value in the object and change.
+                  return { ...item, study: e.target.value };
+                } else {
+                  // If not returned, just leave as is for map has to return something
+                  return item;
+                }
+              }),
+            )
+          }
+          type="text"
+          className="education-input"
+          placeholder="Degree concentration"
+        />
+
+        <label>
+          Enter the date from and until when you attended this school:
+          <br />
+          <label>From</label>
+          <input
+            onChange={(e) =>
+              setEducations((prev) =>
+                prev.map((item, i) => {
+                  if (i === index) {
+                    // Only to CHANGE 1 property: from
+                    // If this is the index in the array of education, take changed value in the object and change.
+                    return { ...item, from: e.target.value };
+                  } else {
+                    // If not returned, just leave as is for map has to return something
+                    return item;
+                  }
+                }),
+              )
+            }
+            type="date"
+            className="profession-input"
+          />
+          <br />
+          <label>To</label>
+          <input
+            onChange={(e) =>
+              setEducations((prev) =>
+                prev.map((item, i) => {
+                  if (i === index) {
+                    // Only to CHANGE 1 property: to
+                    // If this is the index in the array of education, take changed value in the object and change.
+                    return { ...item, to: e.target.value };
+                  } else {
+                    // If not returned, just leave as is for map has to return something
+                    return item;
+                  }
+                }),
+              )
+            }
+            type="date"
+            className="profession-input"
+          />
+        </label>
+
+        {/* Conditional to render add button for education component */}
+        {index === 0 && (
+          <button type="button" onClick={addEducation}>
+            Add
+          </button>
+        )}
+
+        {/* Conditional to render remove button for education component */}
+        {index > 0 && (
+          <button type="button" onClick={removeEducation}>
+            Remove
+          </button>
+        )}
+
+        <Separator />
+      </section>
+    </>
+  );
+}
