21a6674306696e0c9d025b54b1911379621c3056
Mike Nguyen <111297303+cooladx@users.noreply.github.com>
2025-10-19 16:40:12 -0400

    added name, email, and phone to be tracked as an object for react state.

--- src/components/contactInfo/contact.tsx
+++ src/components/contactInfo/contact.tsx
@@ -1,25 +1,65 @@
 import "../../styles/contact.css";
 import Separator from "../line/separator.tsx";
 
-export default function Contact() {
+export type ContactData = {
+  name: string;
+  email: string;
+  phone: string;
+};
+
+export default function Contact({
+  contact,
+  setContact,
+}: {
+  contact: ContactData;
+  setContact: React.Dispatch<React.SetStateAction<ContactData>>;
+}) {
   return (
     <>
       <section className="contact-info">
         <label htmlFor="name">Enter your name:</label>
-        <input type="text" className="contact-input" placeholder="John Smith" />
+        <input
+          type="text"
+          className="contact-input"
+          placeholder="John Smith"
+          onChange={(e) => {
+            setContact((prev) => {
+              const copiedObj = Object.assign({}, prev);
+              copiedObj.name = e.target.value;
+              return copiedObj;
+            });
+          }}
+          value={contact.name}
+        />
         <label htmlFor="email">Enter your email:</label>
         <input
           type="text"
           className="contact-input"
           placeholder="email@gmail.com"
+          onChange={(e) => {
+            setContact((prev) => {
+              const copiedObj = Object.assign({}, prev);
+              copiedObj.email = e.target.value;
+              return copiedObj;
+            });
+          }}
+          value={contact.email}
         />
-        <label htmlFor="email">Enter your phone number:</label>
+        <label htmlFor="phone">Enter your phone number:</label>
         <input
           type="text"
           className="contact-input"
           placeholder="xxx-xx-xxxx"
+          onChange={(e) => {
+            setContact((prev) => {
+              const copiedObj = Object.assign({}, prev);
+              copiedObj.phone = e.target.value;
+              return copiedObj;
+            });
+            console.log(contact);
+          }}
+          value={contact.phone}
         />
-
         <Separator />
       </section>
     </>
