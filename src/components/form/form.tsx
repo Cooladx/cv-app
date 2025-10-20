4374b87d43e3b21509e1fdf002bf46b23cf80c3d
Mike Nguyen <111297303+cooladx@users.noreply.github.com>
2025-10-19 21:23:15 -0400

    adds in profession state uplifted from app.tsx and also maps the arrays of profession sections that a user may make.

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,28 +1,31 @@
-
 import Header from "../title/header.tsx";
 import Contact from "../contactInfo/contact.tsx";
 import Education from "../educationalExperience/education.tsx";
-// import Profession from "../professionalExperience/profession.tsx";
+import Profession from "../professionalExperience/profession.tsx";
 import Finish from "../finish/finish.tsx";
 import "../../styles/form.css";
 import type { EducationData } from "../educationalExperience/education";
 import type { ContactData } from "../contactInfo/contact.tsx";
+import type { professionData } from "../professionalExperience/profession.tsx";
 
 export default function Form({
   education,
   setEducations,
+  profession,
+  setProfessions,
   contact,
   setContact,
   setFormVisability,
 }: {
   education: EducationData[];
   setEducations: React.Dispatch<React.SetStateAction<EducationData[]>>;
+  profession: professionData[];
+  setProfessions: React.Dispatch<React.SetStateAction<professionData[]>>;
   contact: ContactData;
   setContact: React.Dispatch<React.SetStateAction<ContactData>>;
   setFormVisability: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
   // const [professions, setProfessions] = useState([]);
-
 
   return (
     <>
@@ -37,10 +40,19 @@ export default function Form({
             setEducations={setEducations}
           />
         ))}
-        {/* <Education education={education} setEducations={setEducations} /> */}
-        {/* <Profession professions={professions} setProfessions={setProfessions} /> */}
+
+        {profession.map((edu, index) => (
+          <Profession
+            key={index}
+            index={index}
+            profession={edu}
+            setProfessions={setProfessions}
+          />
+        ))}
+
         <Finish
           education={education}
+          profession={profession}
           contact={contact}
           setFormVisability={setFormVisability}
         />
